7a257c3fb6edfde1d76a8493789579d69aede65b
Dumitru <petrov.dumitru@gmail.com>
2016-08-13 17:28:33 +0300

    Create dp_recursive_power.c

---
+++ d04/ex03/dp_recursive_power.c
@@ -0,0 +1,8 @@
+int     dp_recursive_power(int nb, int power)
+{
+    if (power < 0)
+        return (0);
+    if (power == 0)
+        return (1);
+    return (nb * dp_recursive_power(nb, power - 1));
+}
