286c957387e62358f22caac6ba9035585c62be49
Dumitru <petrov.dumitru@gmail.com>
2016-08-13 13:23:01 +0300

    Update dp_atoi.c
    Exact atoi() function.

--- d03/ex08/dp_atoi.c
+++ d03/ex08/dp_atoi.c
@@ -1,1 +1,15 @@
+int ft_atoi(char *str)
+{
+    int     sint;
+    int     i;
 
+    sint = 0;
+    i = 0;
+    while ((str[i] >= 48) && (str[i] < 58))
+    {
+        sint *= 10;
+        sint += str[i] - '0';
+        i++;
+    }
+    return (sint);
+}
