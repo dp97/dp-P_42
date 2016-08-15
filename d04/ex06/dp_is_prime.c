64865d47af9f916da12746160c628ba6c59e4122
Dumitru <petrov.dumitru@gmail.com>
2016-08-15 11:57:18 +0300

    Update dp_is_prime.c

--- d04/ex06/dp_is_prime.c
+++ d04/ex06/dp_is_prime.c
@@ -1,1 +1,10 @@
-int     ft_is_prime(int nb)
+int     dp_is_prime(int nb)
+{
+    if (nb <= 1)
+        return (0);
+    if (nb <= 3)
+        return (1);
+    if (((nb - 1) % 6 == 0) || (nb + 1) % 6 == 0)
+        return (1);
+    return (0);
+}
