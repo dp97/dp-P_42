0b5a0ce9a78764ac802333c465015ade78e52770
Dumitru <petrov.dumitru@gmail.com>
2016-08-15 12:43:53 +0300

    Update dp_is_prime.c

--- d04/ex06/dp_is_prime.c
+++ d04/ex06/dp_is_prime.c
@@ -1,10 +1,19 @@
 int     dp_is_prime(int nb)
 {
+    int     i;
+    
     if (nb <= 1)
         return (0);
-    if (nb <= 3)
+    else if (nb <= 3)
         return (1);
-    if (((nb - 1) % 6 == 0) || (nb + 1) % 6 == 0)
-        return (1);
-    return (0);
+    else if ((nb % 2 == 0) || (nb % 3 == 0))
+        return (0);
+    i = 5;
+    while (i * i <= nb)
+    {
+        if ((nb % i == 0) || (nb % (i + 2) == 0))
+            return (0);
+        i += 6;
+    }
+    return (1);
 }
