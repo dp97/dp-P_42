9f071f8deaf7d3ea6e8b88d857d9630708849d2f
Dumitru <petrov.dumitru@gmail.com>
2016-08-15 13:20:16 +0300

    Update dp_find_next_prime.c

--- d04/ex07/dp_find_next_prime.c
+++ d04/ex07/dp_find_next_prime.c
@@ -2,10 +2,10 @@ int     dp_find_next_prime(int nb)
 {
     int     i;
     
-    if (nb <= 1)
-        return (0);
-    else if (nb <= 3)
-        return (1);
+    if (nb <= 2)
+        return (2);
+    else if (nb == 3)
+        return (3);
     else if ((nb % 2 == 0) || (nb % 3 == 0))
         return dp_find_next_prime(nb + 1);
     i = 5;
