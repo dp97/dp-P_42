221a14b1a096a8029591eaab5159c2bd2ce40f28
Dumitru <petrov.dumitru@gmail.com>
2016-08-17 12:05:40 +0300

    Create colle-03.c

---
+++ Rush00/colle03/colle-03.c
@@ -0,0 +1,25 @@
+void    colle(int x, int y)
+{
+    int     col;
+    int     row;
+    
+    col = 1;
+    while (col <= y)
+    {
+        row = 1;
+        while (row <= x)
+        {
+            if ((col == 1) && ((row == 1) || (row == x)))
+                dp_putchar('A');
+            else if ((col == y) && ((row == 1) || (row == x)))
+                dp_putchar('C');
+            else if ((row > 1) && (row < x) && (col > 1) && (col < y))
+                dp_putchar(' ');
+            else
+                dp_putchar('B');
+            row++;
+        }
+        dp_putchar('\n');
+        col++;
+    }
+}
