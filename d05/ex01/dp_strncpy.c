058d8481650b579c78af6f199d9564164debef6a
Dumitru <petrov.dumitru@gmail.com>
2016-08-19 14:46:18 +0300

    Create dp_strncpy.c

---
+++ d05/ex01/dp_strncpy.c
@@ -0,0 +1,13 @@
+char    *dp_strncpy(char *dest, char *src, unsigned int n)
+{
+    int     i;
+    
+    i = 0;
+    while (i < n)
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
