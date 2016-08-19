cd4890c0c64e7fdbf5d3254e3539be1928e623c0
Dumitru <petrov.dumitru@gmail.com>
2016-08-19 18:45:19 +0300

    Create dp_strcat.c

---
+++ d05/ex13/dp_strcat.c
@@ -0,0 +1,15 @@
+char    *dp_strcat(char *dest, char *src)
+
+{
+    int     i;
+    int     j;
+    
+    i = 0;
+    j = 0;
+    while (dest[i])
+        i++;
+    while (src[j])
+        dest[i++] = src[j++];
+    dest[i] = '\0';
+    return (dest);
+}
