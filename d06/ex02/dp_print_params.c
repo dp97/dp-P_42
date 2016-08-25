2d1fc2b068ed8b82fe6349dc8e64b79f7700cc2f
Dumitru <petrov.dumitru@gmail.com>
2016-08-25 22:03:41 +0300

    Create dp_print_params.c

---
+++ d06/ex02/dp_print_params.c
@@ -0,0 +1,19 @@
+int     main(int ac, char *av[])
+{
+    int     i;
+    int     j;
+    
+    j = 1;
+    while (j < ac)
+    {
+        i = 0;
+        while (av[j][i])
+        {
+            dp_putchar(av[j][i]);
+            i++;
+        }
+        dp_putchar('\n');
+        j++;
+    }
+    return (0);
+}
